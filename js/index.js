5f55a4df89d6ab7271a957a6f7ad518e906b9c3a
Tsuen Hsueh <tsuen@MacBook-Air-a2337.local>
2021-08-14 14:58:20 +0800

    A massive change, all changed

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    var topBox = document.getElementById("topBox");
+    topBox.style.opacity = 1;
+    setTimeout(() => {
+        var navi = document.getElementById("navi");
+        navi.style.opacity = 1;
+    }, 250);
+    setTimeout(() => {
+        var forJS = document.getElementById("forJS");
+        forJS.style.opacity = 1;
+    }, 750);
+};

aef6dedd39ce3064657da07895c0072517ee9160
michael21910 <faefae31910@gmail.com>
2021-09-05 16:08:24 +0800

    change word uva to cpe 1 star codeshare

--- code/js/index.js
+++ code/js/index.js
@@ -30,7 +30,6 @@ solved_cpeOneStar.sort(function compareNumbers(a, b) {
     return a - b;
 })
 
-
 solved_leetcode.sort(function compareNumbers(a, b) {
     return a - b;
 })
@@ -2708,7 +2707,7 @@ function goTop() {
 
 // runs every time when the first select tag changes
 function updateTable(table) {
-    if (table.value === 'uva') {
+    if (table.value === 'cpeOne') {
         // remove the child(options) first
         while (document.getElementById('problemSet').firstChild) {
             document.getElementById('problemSet').removeChild(document.getElementById('problemSet').firstChild)
@@ -2744,7 +2743,7 @@ function updateCode(target){
         for(let i = 0; i < solved_cpeOneStar.length; i++){
             if('u' + solved_cpeOneStar[i] === target.options[target.selectedIndex].value){
                 document.getElementById('codeSpace').innerHTML = UVa_ac[i]
-                document.getElementById('problemTitle').innerHTML = 'UVa' + solved_cpeOneStar[i]
+                document.getElementById('problemTitle').innerHTML = 'CPE 1 star - UVa' + solved_cpeOneStar[i]
             }
         }
     }
